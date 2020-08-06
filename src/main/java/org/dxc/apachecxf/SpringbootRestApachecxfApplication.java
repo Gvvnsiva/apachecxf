dd2db9db76d2caec4155b9af0b92ddfad9882e4a
GUMMELLA <v.gummella@dxc.com>
2020-08-06 17:12:48 +0530

    first commit

---
+++ src/main/java/org/dxc/apachecxf/SpringbootRestApachecxfApplication.java
@@ -0,0 +1,14 @@
+package org.dxc.apachecxf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApachecxfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestApachecxfApplication.class, args);
+		System.out.println("hiiii");
+	}
+
+}
---
+++ src/main/java/org/dxc/apachecxf/exception/PatientNotFoundException.java
@@ -0,0 +1,7 @@
+package org.dxc.apachecxf.exception;
+public class PatientNotFoundException extends RuntimeException{
+/**
+*
+*/
+private static final long serialVersionUID = 1L;
+}
